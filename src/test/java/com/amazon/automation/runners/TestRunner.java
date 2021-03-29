1d0f176c6875666c83652c3dc32d8e1b8f276e0a
Ravindra1498 <ravindraphapale6668@gmail.com>
2021-03-30 00:03:45 +0530

    my first commit

---
+++ src/test/java/com/amazon/automation/runners/TestRunner.java
@@ -0,0 +1,23 @@
+package com.amazon.automation.runners;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features="classpath:features",
+        glue="com.amazon.automation.steps",
+        tags="", 
+        plugin = {
+        	"pretty", 
+            "html:target/html/htmlreport.html",
+            "json:target/json/file.json",
+            },
+        publish=true,
+        dryRun=false 
+        )
+public class TestRunner {
+   
+}
---
+++ src/test/java/com/amazon/automation/steps/StepDefs.java
@@ -0,0 +1,70 @@
+package com.amazon.automation.steps;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+import java.util.concurrent.TimeUnit;
+
+public class StepDefs {
+
+    WebDriver driver;
+    String base_url = "https://amazon.in";
+    int implicit_wait_timeout_in_sec = 20;
+
+ 
+    @Before
+    public void setUp(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(implicit_wait_timeout_in_sec, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void cleanUp(){
+        driver.quit();
+    }
+    
+    @Given("User navigated to the home application url")
+    public void User_navigated_to_the_home_application_url()
+    {
+    	driver.get(base_url);
+    	String expectedTitle = "Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
+    	String acutalTitle = driver.getTitle();
+    	Assert.assertEquals("Home paage Title validation", expectedTitle,acutalTitle);
+    	
+    }
+    
+    @When("User Search for product {string}")
+    public void User_search_for_product(String productName)
+    {
+    	WebDriverWait webDriverWait = new WebDriverWait(driver, 20);
+    	WebElement searchBox = webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("twotabsearchtextbox")));
+    	searchBox.sendKeys(productName);
+    	driver.findElement(By.xpath("//input[@id='nav-search-submit-button']")).click();
+    }
+    @Then("Search Result page is displayed")
+    public void Search_result_page_is_display()
+    {
+    	WebDriverWait webDriverWait2 = new WebDriverWait(driver,20);
+    	webDriverWait2.until(ExpectedConditions.titleIs("Amazon.in : Laptop"));
+    	Assert.assertEquals("page title validation","Amazon.in : Laptop",driver.getTitle());
+    	
+    }
+    
+    
+    
+   
+}
